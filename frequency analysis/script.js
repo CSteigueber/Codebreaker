546861ae67b799127fd12be694e07b33bbf5d51e
CSteigueber <c.steigueber@gmail.com>
2019-11-27 10:26:05 +0100

    shift estimation added not good

--- frequency analysis/script.js
+++ frequency analysis/script.js
@@ -1,5 +1,3 @@
-const str="vwduwljudeehghyhubwklqjlfrxogilqgsohdvhuhwxuqdqbeoxhsulqwviruydxowdqgdodupghvljqedvhgrqzklfkedqnbrxghflghrqldpvhwwlqjxsvdihkrxvhfr";
-
 function countLetters (str){
     let res=[];
     for (let i=0;i<26;i++){
@@ -8,27 +6,36 @@ function countLetters (str){
     for (let i=0; i<str.length; i++){
         for (let j=0; j<26;j++){
             if (str[i]==abc[j]){
-                res[j]++
+                res[j]++;
             }
         }
     }
-    console.log(abc);
-    console.log(res);
-    return res.join(" ");
+    return res;
 }
-const abc="a b c d e f h i j k l m n o p q r s t u v w x y z".split(" ");
+
+const abc="a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
 
 (function main(){
-    document.getElementById("run").addEventListener("click",function(){
-        ;
-        let node=document.createTextNode(countLetters(str));
+    document.getElementById("frequency").addEventListener("click",function(){
+        let str=document.getElementById("input").value;
+        str=countLetters(str);
+        shift=abc.indexOf("e")-str.indexOf(Math.max(str));
+        if (shift<0){
+            shift+=26;
+        }
+        let node=document.createTextNode("Check your console!");
         let p=document.createElement("p");
         p.appendChild(node);
         document.getElementById("output").appendChild(p);
-        node=document.createTextNode(abc.join(" "));
+        node=document.createTextNode(`Estimated shift: ${shift}`);
         p=document.createElement("p");
         p.appendChild(node);
         document.getElementById("output").appendChild(p);
+        let arr=[];
+        for (let i=0;i<str.length;i++){
+            arr.push(`${abc[i]} : ${str[i]}`);
+        }
+        console.log(arr);
     })
 
 })();
