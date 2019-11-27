6c2eb5538faf38a886ab0b33197e1d8a5e80ce32
CSteigueber <c.steigueber@gmail.com>
2019-11-27 10:59:44 +0100

    adding ceasar

--- frequency analysis/script.js
+++ frequency analysis/script.js
@@ -22,6 +22,28 @@ const findMax=(arr)=>{
     return max
 }
 
+const ceasarCipher =(str,key)=>{
+    let res="";
+    for (c in str){
+        let num=c.charCodeAt(0);
+        switch (true){
+            case (((num>=65)&&(num+key<=90))||((num>=97)&&(num+key<=122))): 
+                num+=shift; 
+                break;
+            case (((num-key>=65)&&(num<=90))||((num-key>=97)&&(num<=122))): 
+                num-=shift; 
+                break;
+            case (((num>=65)&&(num<=90))||((num>=97)&&(num<=122))): 
+                num+=(shift-26); 
+                break;
+            case (((num>=65)&&(num<=90))||((num>=97)&&(num<=122))): 
+                num-=(key-26); 
+                break;
+        }
+        res+=String.fromCharCode(num);
+    }
+}
+
 const abc="a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
 
 (function main(){
