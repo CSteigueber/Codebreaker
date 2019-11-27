875306c53c41df88e300cc7352239a3e010faf3e
CSteigueber <c.steigueber@gmail.com>
2019-11-27 10:49:01 +0100

    shift estimation added good

--- frequency analysis/script.js
+++ frequency analysis/script.js
@@ -12,16 +12,26 @@ function countLetters (str){
     }
     return res;
 }
+const findMax=(arr)=>{
+    let max=0;
+    for (let i=0;i<arr.length;i++){
+        if (arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max
+}
 
 const abc="a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
 
 (function main(){
     document.getElementById("frequency").addEventListener("click",function(){
-        let str=document.getElementById("input").value;
-        str=countLetters(str);
-        shift=abc.indexOf("e")-str.indexOf(Math.max(str));
+        const str=document.getElementById("input").value;
+        let res=countLetters(str);
+        shift=abc.indexOf("e")-res.indexOf(findMax(res));
         if (shift<0){
             shift+=26;
+
         }
         let node=document.createTextNode("Check your console!");
         let p=document.createElement("p");
@@ -32,8 +42,8 @@ const abc="a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
         p.appendChild(node);
         document.getElementById("output").appendChild(p);
         let arr=[];
-        for (let i=0;i<str.length;i++){
-            arr.push(`${abc[i]} : ${str[i]}`);
+        for (let i=0;i<res.length;i++){
+            arr.push(`${abc[i]} : ${res[i]}`);
         }
         console.log(arr);
     })
